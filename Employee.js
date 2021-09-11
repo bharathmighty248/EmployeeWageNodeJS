dc5a1028f0b6a6a132dd11112983419f726e7c24
bharathmighty248 <bharathmighty248@gmail.com>
2021-09-11 08:05:53 +0530

    [Bharath]Refactor: Refactored the Code Using Function

--- Employee.js
+++ Employee.js
@@ -1,21 +1,23 @@
 const IS_PART_TIME =1;
-const IS_fULL_tIME =2;
+const IS_FULL_TIME =2;
 const PART_TIME_HOURS =4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
 
+function GetWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
 let empHrs;
 let empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck)
-{
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_fULL_tIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
-}
+empHrs=GetWorkingHours(empCheck);
 let empWage= empHrs*WAGE_PER_HOURS;
 console.log("Employee Wage: "+ empWage);
