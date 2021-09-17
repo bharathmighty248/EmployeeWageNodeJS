5a0e87a2a74df95af2758d24995d7c88ef646056
bharathmighty248 <bharathmighty248@gmail.com>
2021-09-17 16:35:07 +0530

    [Bharath]Add: Arrow Functions Are Implemented

--- Employee.js
+++ Employee.js
@@ -28,6 +28,7 @@ let totalEmpHrs=0;
 let totalWorkingDays=0;
 let empDailyWageArr=new Array();
 let empDailyWageMap=new Map();
+let empDailyHrsMap = new Map();
 
 while(totalEmpHrs<=MAX_HOURS && totalWorkingDays <= WORKING_DAYS)
 {
@@ -36,6 +37,7 @@ while(totalEmpHrs<=MAX_HOURS && totalWorkingDays <= WORKING_DAYS)
     let empHrs = GetWorkingHours(empCheck);
     totalEmpHrs+=empHrs;
     empDailyWageArr.push(CalculateWage(empHrs));
+    empDailyHrsMap.set(totalWorkingDays, empHrs);
     empDailyWageMap.set(totalWorkingDays, CalculateWage(empHrs));
 }
 
@@ -116,5 +118,28 @@ function totalDaysWorked(numOfDays,dailyWage)
         return numOfDays+1;
         return numOfDays;
 }
-console.log("UC-7G : Number of Days the Employee Worked : "+
-            empDailyWageArr.reduce(totalDaysWorked,0));
+console.log("UC-7G : Number of Days the Employee Worked : "+ empDailyWageArr.reduce(totalDaysWorked,0));
+
+console.log(empDailyWageMap);
+console.log("Emp wage map total wages : "+ Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+console.log("Emp wage with arrow fn : Total Hours : "+ totalHours +" total wage : "+totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value, key, map) => {
+    if(value == 8) fullWorkingDays.push(key);
+    else if(value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+
+console.log("Full working days : "+fullWorkingDays);
+console.log("Part working days : "+partWorkingDays);
+console.log("Non working days : "+nonWorkingDays);
